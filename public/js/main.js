556b460b8f2a7ded934ed01af804f05655669792
Azhar <azhrddn.khan@gmail.com>
2022-04-25 10:49:02 +0530

    Weather Checking Website

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById("submitBtn");
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+
+
+const datahide = document.querySelector('.middle_layer');
+
+
+const getInfo = async(event) => {
+    event.preventDefault();
+    let cityVal = cityName.value;
+    
+    if(cityVal === ""){
+        city_name.innerHTML = "Write place name before search";
+        datahide.classList.add('data_hide');
+
+    }else{
+        try{
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=8adacecc4c1e35c78d7e9c8dbbc632ee`;
+            const response = await fetch(url);
+            const data = await response.json();
+          
+            const arrdata = [data];
+
+            city_name.innerHTML = `${arrdata[0].name}, ${arrdata[0].sys.country}`;
+            temp_real_val.innerHTML = arrdata[0].main.temp;
+            // temp_status.innerHTML = arrdata[0].weather[0].main;
+
+            const tempMood = arrdata[0].weather[0].main;
+
+            // condition to check sunny cloudy
+            if(tempMood == "clear"){
+                temp_status.innerHTML = '<i class="fas fa-sun" style="color: #eccc68"></i>';
+            }else if(tempMood ==  'Clouds'){
+                temp_status.innerHTML= '<i class="fas fa-cloud" style="color: #f1f2f6"></i>';
+            }else if(tempMood == "Rain"){
+                temp_status.innerHTML='<i class="fas fa-cloud-rain" style="color: #a4b0be"></i>';
+            }else{
+                temp_status.innerHTML = '<i class="fas fa-sun" style="color: #eccc68"></i>';
+            }
+
+        datahide.classList.remove('data_hide');
+
+        }catch{
+            city_name.innerHTML = "Please enter name properly";
+            datahide.classList.add('data_hide');
+
+        }
+        
+    }
+   
+        
+};
+submitBtn.addEventListener('click', getInfo);
